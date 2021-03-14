4c03c9e0a6fb80db2b0a5aa0f5aa7cf8934a0dfb
pro100kryto <pro100kryto.original@gmail.com>
2021-03-14 15:30:45 -0300

    Add more functionality & refactoring

--- src/main/java/com/pro100kryto/server/modules/DatabaseMongoModule.java
+++ src/main/java/com/pro100kryto/server/modules/DatabaseMongoModule.java
@@ -1,19 +1,25 @@
 package com.pro100kryto.server.modules;
 
+import com.mongodb.MongoException;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.MongoIterable;
+import com.mongodb.client.result.InsertOneResult;
+import com.mongodb.client.result.UpdateResult;
 import com.pro100kryto.server.StartStopStatus;
 import com.pro100kryto.server.logger.ILogger;
 import com.pro100kryto.server.module.AModuleConnection;
 import com.pro100kryto.server.module.Module;
 import com.pro100kryto.server.modules.databasemongo.connection.IDatabaseMongoModuleConnection;
 import com.pro100kryto.server.service.IServiceControl;
-import com.sun.istack.Nullable;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.jetbrains.annotations.Nullable;
 
 public class DatabaseMongoModule extends Module {
     private MongoClient mongoClient = null;
+    private MongoDatabase database = null;
 
     public DatabaseMongoModule(IServiceControl service, String name) {
         super(service, name);
@@ -27,6 +33,10 @@ public class DatabaseMongoModule extends Module {
             mongoClient = MongoClients.create();
         }
 
+        String dbName = settings.getOrDefault("db-name", "");
+        if (dbName.isEmpty()) throw new Throwable("db-name not specified");
+        database = mongoClient.getDatabase(dbName);
+
         if (moduleConnection == null) {
             moduleConnection = new DatabaseMongoModuleConnection(logger, name, type);
         }
@@ -39,7 +49,6 @@ public class DatabaseMongoModule extends Module {
 
     @Override
     public void tick() throws Throwable {
-        Thread.yield();
     }
 
     private final class DatabaseMongoModuleConnection extends AModuleConnection
@@ -54,18 +63,99 @@ public class DatabaseMongoModule extends Module {
             return getStatus() == StartStopStatus.STARTED;
         }
 
-        @Override @Nullable
-        public MongoDatabase getMongoDatabase(final String dbName){
+        @Override
+        public long estimatedCountDocuments(String collectionName){
             try {
-                return mongoClient.getDatabase(dbName);
+                return database.getCollection(collectionName).estimatedDocumentCount();
+            } catch (Throwable ignored){
+            }
+            return -1L;
+        }
+
+        @Override
+        public long countDocuments(String collectionName){
+            try {
+                return database.getCollection(collectionName).countDocuments();
+            } catch (Throwable ignored){
+            }
+            return -1L;
+        }
+
+        @Override
+        public long countDocuments(String collectionName, Bson filter){
+            try {
+                return database.getCollection(collectionName).countDocuments(filter);
+            } catch (Throwable ignored){
+            }
+            return -1L;
+        }
+
+        @Override @Nullable
+        public Document findFirstDocument(String collectionName){
+            try {
+                return database.getCollection(collectionName).find().first();
+            } catch (Throwable ignored){
+            }
+            return null;
+        }
+
+        @Override @Nullable
+        public Document findFirstDocument(String collectionName, Bson filter){
+            try {
+                return database.getCollection(collectionName).find(filter).first();
             } catch (Throwable ignored){
             }
             return null;
         }
 
         @Override
-        public MongoIterable<String> getListDatabaseNames(){
-            return mongoClient.listDatabaseNames();
+        public boolean existCollection(String collectionName){
+            try {
+                database.getCollection(collectionName);
+                return true;
+            } catch (IllegalArgumentException ignored){
+            }
+            return false;
+        }
+
+        @Override
+        public void createCollection(String collectionName){
+            database.createCollection(collectionName);
+        }
+
+        @Override
+        public FindIterable<Document> findDocuments(String collectionName) throws IllegalArgumentException{
+            return database.getCollection(collectionName).find();
+        }
+
+        @Override
+        public FindIterable<Document> findDocuments(String collectionName, Bson filter) throws IllegalArgumentException{
+            return database.getCollection(collectionName).find(filter);
+        }
+
+        @Override
+        public InsertOneResult insertDocument(String collectionName, Document document) throws MongoException {
+            try {
+                try {
+                    return database.getCollection(collectionName).insertOne(document);
+                } catch (IllegalArgumentException ignored){
+                    database.createCollection(collectionName);
+                    return database.getCollection(collectionName).insertOne(document);
+                }
+            } catch (Throwable ignored){
+            }
+
+            return InsertOneResult.unacknowledged();
+        }
+
+        @Override
+        public UpdateResult updateDocument(String collectionName, Bson filter, Bson update) throws MongoException, IllegalArgumentException {
+            return database.getCollection(collectionName).updateOne(filter, update);
+        }
+
+        @Override
+        public UpdateResult replaceDocument(String collectionName, Bson filter, Document document) throws MongoException, IllegalArgumentException {
+            return database.getCollection(collectionName).replaceOne(filter, document);
         }
     }
 }
